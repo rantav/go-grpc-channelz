24ef6e5685949f5c6969220db425a89ce3308959
Ran Tavory <rantav@gmail.com>
2019-08-30 22:37:00 +0300

    Add the admin http server to the demo gRPC service

--- internal/demo/server/main/main.go
+++ internal/demo/server/main/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 
+	"golang.org/x/sync/errgroup"
 	log "google.golang.org/grpc/grpclog"
 
+	channelz "github.com/rantav/go-grpc-channelz"
 	"github.com/rantav/go-grpc-channelz/internal/demo/server"
 )
 
 func main() {
-	bindAddress := ":8080"
-	listener, err := net.Listen("tcp", bindAddress)
+	const (
+		grpcBindAddress  = ":8080"
+		adminBindAddress = ":8081"
+	)
+
+	grpcListener, err := net.Listen("tcp", grpcBindAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	adminListener, err := net.Listen("tcp", adminBindAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,11 +33,16 @@ func main() {
 		log.Fatalf("Failed to create grpc server %+v", err)
 	}
 
-	fmt.Printf("demo server is up is up; bind address: %s \n", bindAddress)
+	// Register the channelz handler
+	channelz.Handle(http.DefaultServeMux, "/")
+
+	g := new(errgroup.Group)
+	g.Go(func() error { return http.Serve(adminListener, nil) })
+	g.Go(func() error { return grpcServer.Serve(grpcListener) })
+
+	fmt.Printf("demo server is up is up; gRPC bind address: %s, http admin address: %s \n", grpcBindAddress, adminBindAddress)
 
 	// should never return
-	err = grpcServer.Serve(listener)
-
+	err = g.Wait()
 	log.Fatalf("Error running server: %+v", err)
-
 }
